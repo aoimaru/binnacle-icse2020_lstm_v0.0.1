a983680cbc3522059092053fcda712a7690cc13e
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-10-20 23:42:49 +0900

    verify refact

--- lstm_v0.0.1/main.py
+++ lstm_v0.0.1/main.py
@@ -23,15 +23,15 @@ VECTOR_SIZE = 10
 def main(args):
     training_indexes = DLSTM_V1._create_training_indexes(target=args[OPTION_01])
     for training_index in training_indexes:
-        print(training_index)
-    # training_x_datas, training_y_datas = DLSTM_V1._create_training_datas(training_indexes=training_indexes, target=args[OPTION_01], vector_size=VECTOR_SIZE)
-    # for vec_size in range(VECTOR_SIZE):
-    #     DLSTM_V1._create_model(
-    #         x_trains=training_x_datas[vec_size], 
-    #         y_trains=training_y_datas[vec_size],
-    #         target=args[OPTION_01],
-    #         dim_id=vec_size
-    #     )
+        pprint.pprint(training_index)
+    training_x_datas, training_y_datas = DLSTM_V1._create_training_datas(training_indexes=training_indexes, target=args[OPTION_01], vector_size=VECTOR_SIZE)
+    for vec_size in range(VECTOR_SIZE):
+        DLSTM_V1._create_model(
+            x_trains=training_x_datas[vec_size], 
+            y_trains=training_y_datas[vec_size],
+            target=args[OPTION_01],
+            dim_id=vec_size
+        )
     # DLSTM_V1._create_model(
     #     x_trains=training_x_datas[0],
     #     y_trains=training_y_datas[0],
